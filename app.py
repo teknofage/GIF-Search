70651a42055d6e55ad385305094d414547b56803
FritzHeider <fritz.heider@students.makeschool.com>
2019-09-17 16:16:00 -0700

    problems redering anything
    having trouble redering anything at all

--- app.py
+++ app.py
@@ -4,31 +4,39 @@ import json
 
 
 app = Flask(__name__)
-@app.route('/')
-
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-   return "Hello, World!"
-   return render_template('index.html')
-   # TODO: Extract the query term from url using request.args.get()
+    """Return Homepage"""
+
    search_term = request.args.get('search_term')
-   # TODO: Make 'params' dictionary containing:
-   params = {"q": search_term, "key": "2T9JR8K6HMUC", "limit": 10}
-   # a) the query term, 'q'
-   # b) your API key, 'key'
-   # c) how many GIFs to return, 'limit'
+  # TODO: Make 'params' dictionary containing:
+  # a) the query term, 'q'
+  # b) your API key, 'key'
+  # c) how many GIFs to return, 'limit'
+      params = {
+      "q": search_term,
+      "key": "UKFHT9WVVN4O",
+      "limit": 10}
+
+
+
+   # TODO: Extract the query term from url using request.args.get()
+
    # TODO: Make an API call to Tenor using the 'requests' library. For
    # reference on how to use Tenor, see:
    # https://tenor.com/gifapi/documentation
    r = requests.get("https://api.tenor.com/v1/search", params = params)
    # TODO: Use the '.json()' function to get the JSON of the returned response
    # object
-   gif_json = r.json()
+   gifs = r.json()
    print (r.json())
    # TODO: Using dictionary notation, get the 'results' field of the JSON,
    # which contains the GIFs as a list
    # TODO: Render the 'index.html' template, passing the list of gifs as a
    # named parameter called 'gifs'
-   return render_template("index.html")
+
+
+   return render_template("index.html", gifs=gifs)
 
 
 
