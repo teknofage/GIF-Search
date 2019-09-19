6cdab561df15a0122dbd826e5301885c7f435f48
FritzHeider <fritz.heider@students.makeschool.com>
2019-09-19 16:02:06 -0700

    update

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ def index():
     params = {"q": search_term, "key": "UKFHT9WVVN4O", "limit": 10}
     r = requests.get("https://api.tenor.com/v1/search", params = params)
     gif_json = r.json()["results"]
-    print (r.json())
+    #print (r.json())
    # TODO: Using dictionary notation, get the 'results' field of the JSON,
    # which contains the GIFs as a list
    # TODO: Render the 'index.html' template, passing the list of gifs as a
    # named parameter called 'gifs'
     gifs = json.loads(r.content)['results']
     return render_template("index.html", gifs=gif_json, search_term = search_term)
-  
-  
+
+
 if __name__ == '__main__':
   app.run(debug=True)
