9aafbb683358a2acd71e9e16308de09ce9b32daa
FritzHeider <fritz.heider@students.makeschool.com>
2019-09-19 14:07:39 -0700

    almost there
    problem with the for loop

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     """Return Homepage"""
-    return render_template("index.html")
+
     search_term = request.args.get('search_term')
   # TODO: Make 'params' dictionary containing:
   # a) the query term, 'q'
@@ -15,7 +15,7 @@ def index():
   # c) how many GIFs to return, 'limit'
     params = {"q": search_term, "key": "UKFHT9WVVN4O", "limit": 10}
     r = requests.get("https://api.tenor.com/v1/search", params = params)
-    gifs = r.json()
+    gifs = r.json()["results"]
     print (r.json())
    # TODO: Using dictionary notation, get the 'results' field of the JSON,
    # which contains the GIFs as a list
